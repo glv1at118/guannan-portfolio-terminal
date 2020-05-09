6e59dc42af3efc3d39d390a9241f0fdeaba0dcc1
Guannan <guannanlv@gmail.com>
2020-05-08 22:26:13 -0400

    Updates

---
+++ precache-manifest.5e8fbd652266a85fd963eee55c9d1175.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d74b82dc796b918cfbc561351bdc81",
+    "url": "/guannan-portfolio-terminal/index.html"
+  },
+  {
+    "revision": "5e1c4f424d30b891ba90",
+    "url": "/guannan-portfolio-terminal/static/css/2.87d4210f.chunk.css"
+  },
+  {
+    "revision": "2b7f303d35a9e633fd2f",
+    "url": "/guannan-portfolio-terminal/static/css/main.b77a4865.chunk.css"
+  },
+  {
+    "revision": "5e1c4f424d30b891ba90",
+    "url": "/guannan-portfolio-terminal/static/js/2.eb40aa8e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/guannan-portfolio-terminal/static/js/2.eb40aa8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b7f303d35a9e633fd2f",
+    "url": "/guannan-portfolio-terminal/static/js/main.bb903960.chunk.js"
+  },
+  {
+    "revision": "852294cc866e17a230c6",
+    "url": "/guannan-portfolio-terminal/static/js/runtime-main.a15f716e.js"
+  },
+  {
+    "revision": "bc54cb7dad13072a2b036183ec9bdbe0",
+    "url": "/guannan-portfolio-terminal/static/media/app-blog.bc54cb7d.jpg"
+  },
+  {
+    "revision": "6d1e53b430960debb43f7f0aca62e14e",
+    "url": "/guannan-portfolio-terminal/static/media/app-botanica.6d1e53b4.jpg"
+  },
+  {
+    "revision": "175c70d55e4cfb103950d6e604eecfcc",
+    "url": "/guannan-portfolio-terminal/static/media/app-dodge.175c70d5.jpg"
+  },
+  {
+    "revision": "9a70b371f350ab985e328e940e2d964e",
+    "url": "/guannan-portfolio-terminal/static/media/app-student.9a70b371.jpg"
+  },
+  {
+    "revision": "d214dad568afd32ffd0a7f4f910664b8",
+    "url": "/guannan-portfolio-terminal/static/media/app-work.d214dad5.jpg"
+  },
+  {
+    "revision": "c951a6adbc8645dd373e874ba2bf7016",
+    "url": "/guannan-portfolio-terminal/static/media/jilin.c951a6ad.png"
+  },
+  {
+    "revision": "c94333e6a0f35741dc87d8dd26abc1a1",
+    "url": "/guannan-portfolio-terminal/static/media/me.c94333e6.jpg"
+  },
+  {
+    "revision": "d7df4335fab77e1c793711c920f96047",
+    "url": "/guannan-portfolio-terminal/static/media/uoft.d7df4335.png"
+  }
+]);

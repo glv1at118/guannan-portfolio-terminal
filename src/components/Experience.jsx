4a8ee9dd44e6222a540cc1c0bc397d42775ebb95
glv1at118 <44102726+glv1at118@users.noreply.github.com>
2020-08-24 16:03:26 -0400

    added new work exp

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,11 +7,22 @@ export default class Experience extends React.Component {
             <ul id="my-exp">
                 <li className="exp-item">
                     <div className="nes-container is-rounded experience with-title">
+                        <p className="title">Front End Developer Intern At Somoplay Inc.</p>
+                        <p className="exp-info">
+                            - Duration: May 2020 - Present<br />
+                            - I worked for Somoplay as a front end developer intern, helped Somoplay build its new mobile-responsive homepage, as well as its admin content management portal<br />
+                            - Technologies: Javascript (ES5/ES6), HTML5, CSS3, Scss, Vue3, Vuex, Vue-Router, TinyMCE, Element UI<br />
+                            - Link: <a href="#" target="_blank" rel="noopener noreferrer">website pre-launch</a>
+                        </p>
+                    </div>
+                </li>
+                <li className="exp-item">
+                    <div className="nes-container is-rounded experience with-title">
                         <p className="title">Programmer Collaborator At Torneo.ca</p>
                         <p className="exp-info">
                             - Duration: May 2019 - September 2019<br />
                             - I worked for Torneo as a front end developer/designer intern, helped Torneo build mobile-responsive blog and sign-in/up pages<br />
-                            - Technologies: Javascript (ES5/ES6), HTML5, CSS3, Vue, Vuex, Vue-Router,Quill.js<br />
+                            - Technologies: Javascript (ES5/ES6), HTML5, CSS3, Vue, Vuex, Vue-Router, Quill.js<br />
                             - Link: <a href="https://www.torneo.ca/info/company" target="_blank" rel="noopener noreferrer">visit company</a>
                         </p>
                     </div>

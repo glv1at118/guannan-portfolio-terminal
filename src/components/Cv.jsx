b28b76ec30cd552c662d032daccfb804531cd247
glv1at118 <44102726+glv1at118@users.noreply.github.com>
2020-08-25 22:27:57 -0400

    updated resume link

--- src/components/Cv.jsx
+++ src/components/Cv.jsx
@@ -6,7 +6,7 @@ export default class Cv extends React.Component {
         return (
             <div id="cv" className="nes-container is-rounded">
                 <i className="far fa-file"></i>
-                <a href="https://drive.google.com/file/d/1eksTvPJr6Wz5KonIurIFCWwIUaJR-71Y/view" target="_blank" rel="noopener noreferrer">
+                <a href="https://drive.google.com/file/d/1812-ab_bUaHH3BKsMFegq-tk5zS-Fhb7/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                     <span className="cv-desktop">see my resume</span>
                     <span className="cv-mobile">cv</span> <i className="fas fa-external-link-alt"></i>
                 </a>
